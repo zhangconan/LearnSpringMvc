ee97f9fd86c94e4db4a29aaa331bbc4583f6cc05
zknxx <zhangconancomputer@126.com>
2016-05-16 20:31:14 +0800

    提交代码

--- src/main/java/com/zkn/learnspringmvc/controller/HomeController.java
+++ src/main/java/com/zkn/learnspringmvc/controller/HomeController.java
@@ -1,6 +1,9 @@
 package com.zkn.learnspringmvc.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,10 +16,13 @@ import org.springframework.web.servlet.ModelAndView;
 public class HomeController {
 
 	@RequestMapping(value="/home.do")
-	public ModelAndView processHomeController(){
+	public String processHomeController(Map<String,Object> modelMap){
+		/**
 		ModelAndView mv = new ModelAndView();
 		mv.addObject("message", "这个是首页!!!");
 		mv.setViewName("home");
-		return mv;
+		*/
+		modelMap.put("message", "这个是首页!!!");
+		return "home";
 	}
 }
--- src/main/webapp/WEB-INF/jsp/home.jsp
+++ src/main/webapp/WEB-INF/jsp/home.jsp
@@ -2,10 +2,10 @@
 	pageEncoding="UTF-8"%>
 <html>
 <head>
-<script type="text/javascript" src=""></script>
-<style type="text/css" style=""></style>
+<script type="text/javascript" src="static/js/jquery-2.1.4.min.js"></script>
 </head>
 <body>
+<%=request.getAttribute("message") %>
 ${message}
 </body>
 </html>
