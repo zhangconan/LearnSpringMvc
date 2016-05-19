1f30b0b328d0e5cbe8461c479eac232434a7c139
zknxx <zhangconancomputer@126.com>
2016-05-19 19:40:04 +0800

    新增BeanNameController

---
+++ src/main/java/com/zkn/learnspringmvc/controller/BeanNameController.java
@@ -0,0 +1,20 @@
+package com.zkn.learnspringmvc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class BeanNameController extends AbstractController{
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("message", "这是第二个首页");
+		mv.addObject("errorMessage", "这是第二个首页错误页面");
+		mv.setViewName("home");
+		return mv;
+	}
+}
